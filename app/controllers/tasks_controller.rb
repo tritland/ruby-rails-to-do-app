a44e6489c27a0be7f62267938fc5aa3f1401bb23
Tom Ritland <t_ritland@yahoo.com>
2017-11-25 21:45:51 -0600

    delete route works - but still bootstrap issues with delete icon

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,18 @@ class TasksController < ApplicationController
     def new
         @task = Task.new
     end
+
     def create
         @task = Task.create(task_params)
         @tasks = Task.all
     end
+
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        @tasks = Task.all
+    end
+
     private
     def task_params
         params.require(:task).permit(:title, :note, :completed)
