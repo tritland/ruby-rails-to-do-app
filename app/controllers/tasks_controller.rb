31e6f3bc807c637c9bd986d8e6d08710ef666f3e
Tom Ritland <t_ritland@yahoo.com>
2017-11-25 22:44:45 -0600

    update route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,30 @@
 class TasksController < ApplicationController
     def new
         @task = Task.new
+        render :show_form
     end
 
     def create
         @task = Task.create(task_params)
         @tasks = Task.all
+        render :hide_form
     end
 
     def destroy
         @task = Task.find(params[:id])
         @task.destroy
         @tasks = Task.all
+    end
+
+    def edit
+        @task = Task.find(params[:id])
+        render :show_form
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update_attributes(task_params)
+        render :hide_form
     end
 
     private
